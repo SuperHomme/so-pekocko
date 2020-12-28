47fdfcd0565920807336e7a1d77e8a090b7f38e1
Ugo Pellissier de Féligonde <ugo.defeli@gmail.com>
2020-12-28 23:03:51 +0100

    controllers = ok

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,4 +1,5 @@
 const Sauce = require('../models/Sauce'); // on localise le fichier "model" Sauce
+const fs = require('fs');
 
 exports.addSauce = (req, res, next) => {
   const sauceObject = JSON.parse(req.body.sauce);
@@ -8,7 +9,7 @@ exports.addSauce = (req, res, next) => {
     imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
   });
   sauce.save()
-    .then(sauce => res.status(201).json({ message: 'Sauce enregistrée'}))
+    .then(sauce => res.status(201).json({ message: 'sauce enregistrée'}))
     .catch(error => res.status(400).json({ error }));
 };
 
@@ -25,9 +26,26 @@ exports.getOneSauce = (req, res, next) => {
 };
 
 exports.updateSauce = (req, res, next) => {
+  const sauceObject = req.file ? {
+    ...JSON.parse(req.body.sauce),
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  } : { ...req.body };
+  Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+    .then(sauce => res.status(200).json({ message: 'sauce mise à jour'}))
+    .catch(error => res.status(400).json({ error }));
 };
 
 exports.deleteSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then(sauce => {
+      const filename = sauce.imageUrl.split('/images/')[1];
+      fs.unlink(`images/${filename}`, () => {
+        Sauce.deleteOne({ _id: req.params.id })
+          .then(sauce => res.status(200).json({ message: 'sauce supprimée'}))
+          .catch(error => res.status(400).json({ error }));
+      });
+    })
+    .catch(error => res.status(500).json({ error }));
 };
 
 exports.likeSauce = (req, res, next) => {
--- routes/sauce.js
+++ routes/sauce.js
@@ -9,7 +9,7 @@ router.get('/', auth, sauceCtrl.getAllSauces);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.post('/', auth, multer, sauceCtrl.addSauce);
 router.put('/:id', auth, multer, sauceCtrl.updateSauce);
-router.delete('/:id', auth, multer, sauceCtrl.deleteSauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
 router.post('/:id/like', auth, sauceCtrl.likeSauce);
 
 module.exports = router;
