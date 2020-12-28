e0ddeb96ad178908f3670f8b6aae78101c7d9d6c
Ugo Pellissier de Féligonde <ugo.defeli@gmail.com>
2020-12-29 00:16:39 +0100

    ajout de MIME TYPES

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,9 +1,11 @@
 const multer = require('multer');
 
 const MIME_TYPES = {
-  'image/jpg': 'jpg',
-  'image/jpeg': 'jpg',
-  'image/png': 'png'
+    "image/jpg": "jpg",
+    "image/jpeg": "jpeg",
+    "image/png": "png",
+    "image/webp": "webp",
+    "image/gif": "gif"
 };
 
 const storage = multer.diskStorage({
